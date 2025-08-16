3836c983adce1c3f951b6ba836b237ed1233528b
Sure-dn <sspremalatha3@gmail.com>
2025-08-16 22:46:18 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// TODO: Replace with your Firebase config
+const firebaseConfig = {
+  apiKey: "<YOUR_API_KEY>",
+  authDomain: "<YOUR_PROJECT_ID>.firebaseapp.com",
+  projectId: "<YOUR_PROJECT_ID>",
+  storageBucket: "<YOUR_PROJECT_ID>.appspot.com",
+  messagingSenderId: "<YOUR_SENDER_ID>",
+  appId: "<YOUR_APP_ID>"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Example: display a message
+document.getElementById('content').innerText = 'Firebase is initialized!';
